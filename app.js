5e535486d6f3f9e0fd7f343d10eeaa3862c5adf0
evadhankhar <eva.dhankhar.9@gmail.com>
2020-07-01 18:35:14 +0530

    comments

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-// load the things we need
 var express = require('express');
 var app = express();
-
-// set the view engine to ejs
 app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
-// index page 
 app.get('/', function(req, res) {
 	res.render('pages/index');
-	// res.redirect('https://google.com')
 });
-
-// about page 
 app.get('/about', function(req, res) {
 	res.render('pages/about');
 });
